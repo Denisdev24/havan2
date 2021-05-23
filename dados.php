407d695edf174367e0632613a17b9d1cf4c42c70
Denis <ausdenis@hotmail.com>
2021-05-23 13:12:45 -0300

    Segundo commit

--- dados.php
+++ dados.php
@@ -1,4 +1,5 @@
 <?php
+//Receber os valores
 $caso = $_POST['caso'];
 $nome=$_POST['nome'];
 $valor=$_POST['valor']; 
@@ -7,45 +8,58 @@ $resultfim=($taxa - $valor);
 
 echo "Nome  do Cliente:" . $nome;
 echo "</br>";
-if ($valor == 0 ){
-    header('location:Sistema.php');
+// validação..
+if ($valor ==0 || $nome ==0){
+    header('location:sistema.php');
 }
 else{
     switch ($caso){
         case 1:
-            $conta= $resultfim / 5.29;
-            $result = number_format($conta,2);
-        echo  $result . " Dolares";
+            $conta= ($resultfim / 5.29) * -1;
+            $result =number_format($conta,2);
+        echo "Valor convertido:" . $result . " Dólares";
         echo "</br>";
-        echo "Taxa cobrada:" . $taxa ."Reais";
+        echo "Taxa cobrada:" . $taxa ." Reais";
             break;
         case 2:
-            $conta = $resultfim*5.29;
+            $conta = ($resultfim *5.29) *-1;
             $result= number_format($conta,2);
-            echo $result . " Reais";
+            echo "Valor convertido:" . $result . " Reais";
+            echo "</br>";
+            echo "Taxa cobrada:" . $taxa ." Euro";
             break;
         case 3:
-            $conta = $resultfim *1.22;
+            $conta = ($resultfim *1.22) * -1;
             $result=number_format($conta,2);
-            echo $result .  " Dolares";
+            echo "Valor covertido:" .$result .  " Euros";
+            echo "</br>";
+            echo "Taxa cobrada:" . $taxa ." Dólares";
             break;
         case 4 :
-            $conta= $resultfim / 1.22;
+            $conta= ($resultfim / 1.22) * -1;
             $result=number_format($conta,2);
-            echo $result . " Euros";
+            echo "Valor convertido:" .$result . " Dólares";
+            echo "</br>";
+            echo "Taxa cobrada:" . $taxa ." Euro";
             break;
         case 5 :
-            $conta =$resultfim/ 0.154;
+            $conta =($resultfim/ 0.154) * -1;
             $result= number_format($conta,2);
-            echo $result . " Reais";
+            echo "Valor convertido:".$result . " Reais";
+            echo "</br>";
+            echo "Taxa cobrada:" . $taxa ." Euros";
+            echo "</br>";
             break;
         case 6 :
-            $conta = $resultfim * 0.154;
+            $conta = ($resultfim * 0.154) * -1 ;
             $result= number_format($conta,2); 
-            echo $result . " Euros";
+            echo "Valor convertido:".$result . " Euros";
+            echo "</br>";
+            echo "Taxa cobrada:" . $taxa ." Reais";
         }
         }
       
+        echo "</br>";
         echo "</br>";
         echo "*  Data da cotação utilizada : 21/05/2021 ";
 ?>
